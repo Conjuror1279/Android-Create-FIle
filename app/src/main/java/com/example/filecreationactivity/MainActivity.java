57b792e0833340031dd081bbca26ed8ca34fe1dd
Mridul <mridul.natani@navi.com>
2022-06-24 00:03:12 +0530

    permissions

--- app/src/main/java/com/example/filecreationactivity/MainActivity.java
+++ app/src/main/java/com/example/filecreationactivity/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.filecreationactivity;
 import static android.widget.Toast.LENGTH_LONG;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
@@ -14,16 +20,25 @@ import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mBtnCreateFile;
     private TextView notificationView;
+    private Context mContext;
+    private static final int REQUEST_PERMISSION_CODE = 12345;
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[] {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContext = getApplicationContext();
         initUI(getApplicationContext());
     }
 
@@ -34,19 +49,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 //                File fileFolder = new File(Environment.getDataDirectory(), "PlayBackShots");
-                File videoFile = new File("VideoView.mp4");
+                checkAndRequestPermissions();
+                File videoFile = new File(Environment.getExternalStorageDirectory().
+                        getPath() + "droame.mp4");
+                notificationView.setText(getApplicationContext().getFilesDir().getAbsolutePath() + "VideoView.mp4");
                 if(!videoFile.exists()) {
                     try {
                         videoFile.createNewFile();
-                        Toast.makeText(getApplicationContext(), "File Created Successfully!", LENGTH_LONG);
-                        notificationView.setText("File Created Successfully");
+                        Toast.makeText(MainActivity.this, "File Created Successfully!", LENGTH_LONG);
+                        notificationView.setText("file created successfully");
                     } catch (IOException e) {
-                        Toast.makeText(getApplicationContext(), e.toString(), LENGTH_LONG);
+                        Toast.makeText(MainActivity.this, e.toString(), LENGTH_LONG);
                         notificationView.setText("Error" + e.toString());
                     }
                 }
 
             }
         });
+    }
+
+    private void checkAndRequestPermissions() {
+        // Check for permissions
+        List<String> missingPermission = new ArrayList<>();
+        for (String eachPermission : REQUIRED_PERMISSION_LIST) {
+            if (ContextCompat.checkSelfPermission(mContext, eachPermission) != PackageManager.PERMISSION_GRANTED) {
+                missingPermission.add(eachPermission);
+            }
+        }
+        // Request for missing permissions
+        if (missingPermission.isEmpty()) {
+//            Toast.setResultToToast("Permissions Checked!");
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this,
+                    missingPermission.toArray(new String[missingPermission.size()]),
+                    REQUEST_PERMISSION_CODE);
+        }
+
     }
 }
